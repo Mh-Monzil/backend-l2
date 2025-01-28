a7ae5b63c4a23723d8ec609d54f6de65329f810f
Mh-Monzil <monzil246@gmail.com>
2025-01-29 00:22:49 +0600

    update: student schema

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,38 @@
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type Username = {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+};
+
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type Student = {
+  id: string;
+  name: Username;
+  gender: 'male' | 'female';
+  email: string;
+  dateOfBirth?: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  presentAddress: string;
+  permanentAddress: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImage?: string;
+  isActive: boolean;
+};
