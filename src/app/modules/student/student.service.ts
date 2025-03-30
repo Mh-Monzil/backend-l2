6e420e81a1457d3317396ee26cc2bf71bb606b27
Mh-Monzil <monzil246@gmail.com>
2025-03-30 19:49:38 +0600

    add: pagination

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -21,7 +21,7 @@ const getAllStudentFromDB = async (query: Record<string, unknown>) => {
   });
 
   // Filtering
-  const excludeFields = ['searchTerm', 'sort', 'limit'];
+  const excludeFields = ['searchTerm', 'sort', 'limit', 'page'];
   excludeFields.forEach((field) => delete queryObj[field]);
 
   const filterQuery = searchQuery
@@ -41,12 +41,22 @@ const getAllStudentFromDB = async (query: Record<string, unknown>) => {
 
   const sortQuery = filterQuery.sort(sort);
 
+  let page = 1;
   let limit = 1;
+  let skip = 0;
+
   if (query.limit) {
-    limit = query.limit as number;
+    limit = Number(query.limit);
   }
 
-  const limitQuery = await sortQuery.limit(limit);
+  if (query.page) {
+    page = Number(query.page);
+    skip = (page - 1) * limit;
+  }
+
+  const paginateQuery = sortQuery.skip(skip);
+
+  const limitQuery = await paginateQuery.limit(limit);
 
   return limitQuery;
 };
